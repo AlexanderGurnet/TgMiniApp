60d6f3f0ab72980725e9970d5251fd3617d9e0ca
Alexander Gurnet <Alexandergurnet@gmail.com>
2024-05-22 23:51:52 +0200

    add tonconnect manifest and @tonconnect/ui-react

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import WebApp from '@twa-dev/sdk'
+import { TonConnectUIProvider } from '@tonconnect/ui-react'
 import App from './App.jsx'
 import './index.css'
 
@@ -8,6 +9,8 @@ WebApp.ready()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TonConnectUIProvider manifestUrl="https://alexandergurnet.github.io/TgMiniApp/tonconnect-manifest.json">
+      <App />
+    </TonConnectUIProvider>
   </React.StrictMode>
 )
