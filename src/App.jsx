f2a56451639f8608239012cc87480b40f9a67a04
Alexander Gurnet <Alexandergurnet@gmail.com>
2024-05-23 01:43:10 +0200

    fix to the return strategy 9

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,8 @@ import { TonConnectButton, useTonConnectUI } from '@tonconnect/ui-react'
 import { Address } from './Address'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-      </div>
       {/* Here we add our button with alert callback */}
       <div className="card">
         <TonConnectButton />
